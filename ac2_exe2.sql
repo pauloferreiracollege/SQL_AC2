a7b14e62592036b243a121e87eddb6440a7fc79d
Paulo Mobile <bale_paulo@Outlook.com>
2019-03-10 22:45:11 -0300

    Changing spacing

--- ac2_exe2.sql
+++ ac2_exe2.sql
@@ -3,28 +3,28 @@ CREATE DATABASE ac2_biblioteca
 USE ac2_biblioteca
 
 CREATE TABLE Livro(
-Nome		VARCHAR(200) NOT NULL,
-Copia		INT,
-StatusLivro VARCHAR(10)  NOT NULL,
+Nome VARCHAR(200) NOT NULL,
+Copia INT,
+StatusLivro VARCHAR(10) NOT NULL,
 CONSTRAINT PK_Livro PRIMARY KEY (Nome, Copia)
 )
 
 CREATE TABLE Cliente(
-Id		 INT		  PRIMARY KEY,
-Nome	 VARCHAR(200) NOT NULL,
-Telefone INT          NOT NULL
+Id INT PRIMARY KEY,
+Nome VARCHAR(200) NOT NULL,
+Telefone INT NOT NULL
 )
 
 DROP TABLE 
 CREATE TABLE Emprestimo(
-Id				      INT IDENTITY PRIMARY KEY,
-NomeLivro			  VARCHAR(200) NOT NULL,
-CopiaLivro			  INT		   NOT NULL,
-IdCliente			  INT          NOT NULL,
-DataEmprestimo		  VARCHAR(20)  NOT NULL,
-DataDevolucaoProposta VARCHAR(20)  NOT NULL,
-DataDevolucaoEfetiva  VARCHAR(20)  NOT NULL,
-Multa				  DECIMAL,
+Id INT IDENTITY PRIMARY KEY,
+NomeLivro VARCHAR(200) NOT NULL,
+CopiaLivro INT NOT NULL,
+IdCliente INT NOT NULL,
+DataEmprestimo VARCHAR(20) NOT NULL,
+DataDevolucaoProposta VARCHAR(20) NOT NULL,
+DataDevolucaoEfetiva VARCHAR(20) NOT NULL,
+Multa DECIMAL
 )
 
 ALTER TABLE Emprestimo
